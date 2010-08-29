a9407ce8d0072fa0b21824db4c39bc58234136ad
Marak Squires <marak.squires@gmail.com>
2010-08-29 19:23:27 -0400

    accordion now autoexpands first item

--- client/js/app.js
+++ client/js/app.js
@@ -226,6 +226,8 @@ $(function() {
                     ev.stopPropagation();
                 });
                 
+                $("#featureslist, .scenario").accordion( "activate" , 0 );
+                
                 $("#featureslist input").mouseover(function() {
                   $(this).addClass("inlineEditHover");
                 }).mouseout(function() {
