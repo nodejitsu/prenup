0d2d64f5a8da03b3e7fccf2f826c13fc23d4fd1f
hij1nx <fernmicro@gmail.com>
2010-08-29 16:20:42 -0400

    metro

--- client/js/app.js
+++ client/js/app.js
@@ -114,6 +114,22 @@ $(function() {
                 
                 // some dialogs
                 
+                var splash = $($.jup.html(
+                  ["div", { "id": "splash" },
+                    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
+                  )).dialog({
+                  resizable: false,
+                  width: 655,
+                  height: 400,
+                  modal: true,
+                  dialogClass: "shadow",             
+                  buttons: {
+                    "ok": function() {
+                      $(this).dialog("close");
+                    }
+                  }
+                });                
+                
                 var exportAction = $($.jup.html(["div", "This will export your stuff someplace..."])).dialog({
                   resizable: false,
                   autoOpen: false,
@@ -176,15 +192,17 @@ $(function() {
                 var html = [];
 
                 $.each(DAL.get.milestones(), function(key, milestone) {
-                    html.push($.jup.html([
+                    html.push($.jup.html(["li",
                         ["input", { "class": "btn", "id": "ms" + key, "type": "checkbox" }], 
-                        ["label", { "class": "milestone", "for": "ms" + key }, milestone + "<span class='remove remove-milestone'></span>",
-                                                  
+                        ["label", { "class": "milestone", "for": "ms" + key }, milestone,
+                          ["img", {"src": "img/delete.png", "height": "22", "width": "22" }]   
                         ]
                     ]));
                 });
+                
+                html.push()
 
-                $("#toolbar").html(html.join("")).disableSelection();
+                $("#toolbar ul").html(html.join("")).disableSelection().sortable();
                 $("#toolbar .btn").button().click(function() {
                   
                   $("h3.milestone-member." + $(this).attr("id"))[$(this).attr("checked") ? "fadeIn" : "fadeOut"]();
